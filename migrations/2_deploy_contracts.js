eb49e49ee4226698ee4df56623481379d22dafab
Rishita Shaw <rishitashaw49@gmail.com>
2021-09-01 13:26:55 +0530

    dl

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-//Import Contractconst DStorage = artifacts.require("DStorage");
-const Wedding = artifacts.require("Wedding");
-
-module.exports = function (deployer) {
-  //Deploy Contract
-  deployer.deploy(Wedding);
-};
