c67025b52f9b4fb51325c2cdd5daea34bdb58090
Moinak Banerjee <moinak878@gmail.com>
2021-09-01 13:45:05 +0530

    i cri

--- packages/client/src/pages/dapp.js
+++ packages/client/src/pages/dapp.js
@@ -11,51 +11,43 @@ import { LitElement, html, customElement, property } from "lit-element";
 
 @customElement("dapp-page")
 export default class DappPage extends LitElement {
-  @property()
-  get;
-  @property()
-  post;
-  @property()
-  title;
-  @property()
-  category;
-  @property()
-  description;
+	@property()
+	get;
+	@property()
+	post;
+	@property()
+	title;
+	@property()
+	category;
+	@property()
+	description;
 
-  createRenderRoot() {
-    return this;
-  }
-  constructor(args) {
-    super(args);
-  }
+	createRenderRoot() {
+		return this;
+	}
+	constructor(args) {
+		super(args);
+	}
 
-
-  render() {
-    let content = html`
-      <div class="container m-auto">
-        <div class="row fadeIn mt-3 p-2 block">
-          <h2 class="text-6xl">🎉 SolVows</h2>
-          <p class="mt-3">
-            Your Dapp is ready, and the world is waiting for you to create
-            something amazing.
-          </p>
-          <p class="mt-3">
-            You are currently viewing your Dapp in the DappStarter Workspace which 
-            provides an easy way to switch between the Client, UI Harness, Customizer and Server 
-            web apps. You can view any of these in a separate browser tab by clicking the 
-            <svg style="display:inline;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon feather feather-arrow-up-right"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg> 
-            icon at the top right of the frame (try it now).
-          </p>
-          <h3 class="mt-3 text-3xl">Next Steps</h3>
-          <ul class="mt-3 ml-5 list-decimal">
-            <li class="mt-3">Customize modules that support customization using the Customizer</li>
-            <li class="mt-3">Interact with the feature modules you selected with the UI Harness</li>
-            <li class="mt-3">Customize this page by editing <i>packages/client/src/pages/dapp.js</i></li>
-          </ul>
-        </div>
-      </div>
-    `;
-    return content;
-
-  }
+	render() {
+		let content = html`
+			<div class="container m-auto">
+				<div class="row fadeIn mt-3 p-2 block">
+					<h2 class="text-6xl">🎉 SolVows</h2>
+					<p class="mt-3">
+						Get married on the Blockchain! ows are an incredibly meaningful—and
+						personal—part of a wedding ceremony. They provide the contractual
+						(both formal and informal) basis that will guide the newlyweds
+						through their shared life together and set the tone for what is to
+						come. While there are many creative variations of the lifelong
+						promises, some couples prefer to recite powerful, centuries-old
+						wedding vows from many different faiths and cultures. These
+						tried-and-true words set a strong foundation for a lifetime of unity
+						and marital bliss together.
+					</p>
+				</div>
+			</div>
+		`;
+		return content;
+	}
 }
